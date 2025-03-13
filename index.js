f6edb40d6a74d8965dae682087641d1582095644
sahash07 <raikapilkhutru9863329002@gmail.com>
2025-03-13 19:55:35 +0545

    2025-03-13T19:55:35+05:45

---
+++ index.js
@@ -0,0 +1,20 @@
+const jsonfile=require('jsonfile');
+const moment=require('moment');
+const simpleGit=require('simple-git');
+
+
+
+const PATH_FILE='./data.json';
+const DATE=moment().format();
+
+const data={
+    date: DATE
+}
+
+jsonfile.writeFile(PATH_FILE, data);
+
+simpleGit().add([PATH_FILE]).commit(DATE,{'--date': DATE}).push();
+
+
+
+
